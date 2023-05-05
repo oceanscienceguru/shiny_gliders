c6be1ff05264219fe60ebb5c3595c80cf60cd573
Sukerfish <viper31jas@aim.com>
2023-05-05 16:37:29 -0400

    initial file

---
+++ modules/routing.R
@@ -0,0 +1,210 @@
+##### Main glider dashboard module #########
+library(shiny)
+
+routing_ui <- function(id) {
+  
+  ns <- NS(id)
+  
+  tagList(
+    h2("Route Planning"),
+    p("Red circles denote points in selected goto file. Radius of circle is considered as achieved."),
+    box(width=12,
+      leafletOutput(outputId = ns("routingMap")) %>% withSpinner(color="#0dc5c1")
+    ),
+    fluidRow(
+    selectInput(inputId = ns("gotoFile"),
+                label = "Which goto file to load?",
+                choices = c("user upload", routesList_files$names),
+                selected = tail(routesList_files$names, 1)),
+    fileInput(ns("gotoTest"), "Load goto file",
+              multiple = FALSE,
+              accept = c(".ma"))
+    )
+    )
+}
+
+routing_server <- function(id, gliderName) {
+  
+  moduleServer(id, function(input, output, session) {
+    
+    load(paste0("/echos/", gliderName, "/glider_live.RData"))
+    
+    #massage gps data a lot
+    map_sf <- gliderdf %>%
+      select(m_present_time, m_gps_lon, m_gps_lat) %>%
+      filter(!is.na(m_gps_lat)) %>%
+      mutate(latt = format(m_gps_lat, nsmall = 4),
+             longg = format(m_gps_lon, nsmall = 4)) %>% #coerce to character keeping zeroes out to 4 decimals
+      separate(latt, paste0("latt",c("d","m")), sep="\\.", remove = FALSE) %>% #have to double escape to sep by period
+      separate(longg, paste0("longg",c("d","m")), sep="\\.", remove = FALSE) %>%
+      mutate(latd = substr(lattd, 1, nchar(lattd)-2), #pull out degrees
+             longd = substr(longgd, 1, nchar(longgd)-2)) %>%
+      mutate(latm = paste0(str_sub(lattd, start= -2),".",lattm), #pull out minutes
+             longm = paste0(str_sub(longgd, start= -2),".",longgm)) %>%
+      mutate_if(is.character, as.numeric) %>% #coerce back to numeric
+      mutate(lat = latd + (latm/60),
+             long = (abs(longd) + (longm/60))*-1) #*-1 for western hemisphere
+    
+    gotoFiles <- toGliderList %>%
+      filter(str_ends(fileName, "goto_l10.ma")) %>%
+      arrange(fileName)
+    
+    #get commanded wpt
+    cwpt <- gliderdf %>%
+      select(m_present_time, c_wpt_lat, c_wpt_lon) %>%
+      filter(!is.na(c_wpt_lat)) %>%
+      select(!c(m_present_time)) %>%
+      format(., nsmall = 4) %>% #coerce to character keeping zeroes out to 4 decimals
+      tail(1)  %>% 
+      separate(c_wpt_lat, paste0("latt",c("d","m")), sep="\\.", remove = FALSE) %>% #have to double escape to sep by period
+      separate(c_wpt_lon, paste0("longg",c("d","m")), sep="\\.", remove = FALSE) %>%
+      mutate(latd = substr(lattd, 1, nchar(lattd)-2), #pull out degrees
+             longd = substr(longgd, 1, nchar(longgd)-2)) %>%
+      mutate(latm = paste0(str_sub(lattd, start= -2),".",lattm), #pull out minutes
+             longm = paste0(str_sub(longgd, start= -2),".",longgm)) %>%
+      mutate_if(is.character, as.numeric) %>% #coerce back to numeric
+      mutate(lat = latd + (latm/60),
+             long = (abs(longd) + (longm/60))*-1) #*-1 for western hemisphere
+    
+    gotoN <- as.integer(nrow(gotoFiles))
+    
+    #build goto history
+    gotoHistory <- list()
+    for (i in 1:gotoN) {
+      gotoHistory[[i]] <- gotoLoad(paste0("/gliders/gliders/", gliderName, "/archive/", gotoFiles[i,]))
+    }
+    
+    #get most recent goto file
+    goto <- as.data.frame(tail(gotoHistory, 1))
+    
+    routingMap <- reactive({
+      
+      if(input$gotoFile == "user upload"){
+        
+        file <- input$gotoTest
+        req(file)
+        # print(file)
+        # print(file$datapath)
+        
+        userGoto <- gotoLoad(file$datapath)
+        
+        #print(userGoto)
+        #base provider layers
+        p <- leaflet() %>%
+          addProviderTiles(providers$Esri.OceanBasemap,
+                         group = "Ocean Basemap") %>%
+          addProviderTiles(providers$Esri.WorldImagery,
+                           group = "World Imagery") %>%
+          addLayersControl(baseGroups = c('Ocean Basemap', 'World Imagery')) %>%
+          addPolylines(
+            lat = map_sf$lat,
+            lng = map_sf$long,
+            color = "grey",
+            weight = 3,
+            opacity = 1,
+          ) %>%
+          #timestamps for surfacings
+          addCircles(data = map_sf,
+                     lat = map_sf$lat,
+                     lng = map_sf$long,
+                     color = "gold",
+                     popup = map_sf$m_present_time,
+                     weight = 3
+          ) %>%
+          #start marker
+          addAwesomeMarkers(
+            lat = map_sf[1, "lat"],
+            lng = map_sf[1, "long"],
+            label = "Initial position",
+            icon = icon.start
+          ) %>%
+          #end marker
+          addAwesomeMarkers(
+            lat = map_sf[nrow(map_sf), "lat"],
+            lng = map_sf[nrow(map_sf), "long"],
+            label = "Latest position",
+            icon = icon.latest
+          ) %>%
+          #waypoints, list first, then add current commanded
+          addCircles(lat = goto$lat,
+                     lng = goto$long,
+                     radius = goto$rad,
+                     label = goto$comment) %>%
+          addMarkers(lat = cwpt$lat,
+                     lng = cwpt$long,
+                     label = "Commanded wpt") %>%
+          addMeasure(primaryLengthUnit = "kilometers",
+                     secondaryLengthUnit = "miles") %>%
+          addCircles(lat = userGoto$lat,
+                     lng = userGoto$long,
+                     radius = userGoto$rad,
+                     label = userGoto$comment,
+                     color = "red")
+        
+        p
+      } else {
+      
+    p <- leaflet() %>%
+      #base provider layers
+      addProviderTiles(providers$Esri.OceanBasemap,
+                       group = "Ocean Basemap") %>%
+      addProviderTiles(providers$Esri.WorldImagery,
+                       group = "World Imagery") %>%
+      addLayersControl(baseGroups = c('Ocean Basemap', 'World Imagery')) %>%
+      addPolylines(
+        lat = map_sf$lat,
+        lng = map_sf$long,
+        color = "grey",
+        weight = 3,
+        opacity = 1,
+      ) %>%
+      #timestamps for surfacings
+      addCircles(data = map_sf,
+                 lat = map_sf$lat,
+                 lng = map_sf$long,
+                 color = "gold",
+                 popup = map_sf$m_present_time,
+                 weight = 3
+      ) %>%
+      #start marker
+      addAwesomeMarkers(
+        lat = map_sf[1, "lat"],
+        lng = map_sf[1, "long"],
+        label = "Initial position",
+        icon = icon.start
+      ) %>%
+      #end marker
+      addAwesomeMarkers(
+        lat = map_sf[nrow(map_sf), "lat"],
+        lng = map_sf[nrow(map_sf), "long"],
+        label = "Latest position",
+        icon = icon.latest
+      ) %>%
+      #waypoints, list first, then add current commanded
+      addCircles(lat = goto$lat,
+                 lng = goto$long,
+                 radius = goto$rad,
+                 label = goto$comment) %>%
+      addMarkers(lat = cwpt$lat,
+                 lng = cwpt$long,
+                 label = "Commanded wpt") %>%
+      addMeasure(primaryLengthUnit = "kilometers",
+                 secondaryLengthUnit = "miles") %>%
+      addCircles(lat = routesList[[input$gotoFile]]$lat,
+                 lng = routesList[[input$gotoFile]]$long,
+                 radius = routesList[[input$gotoFile]]$rad,
+                 label = routesList[[input$gotoFile]]$comment,
+                 color = "red")
+
+    }
+    })
+    
+    output$routingMap <- renderLeaflet({
+      routingMap()})
+    
+})
+}
+  
+  
+    
+
