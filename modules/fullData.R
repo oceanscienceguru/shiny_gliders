07700698d93a43e10c5893d896fbdab23f1e5031
Sukerfish <viper31jas@aim.com>
2023-06-22 13:01:28 -0400

    switch from provider tiles to WMS layer tiles for Esri ocean basemap

--- modules/fullData.R
+++ modules/fullData.R
@@ -326,11 +326,22 @@ fullData_server <- function(id) {
       
       leaflet() %>%
         #base provider layers
-        addProviderTiles("Esri.OceanBasemap", 
-                         group = "Ocean Basemap") %>%
-        addProviderTiles("Esri.WorldImagery", 
+        addWMSTiles("https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}.png",
+                    layers = "World_Ocean_Base",
+                    group = "Ocean Basemap",
+                    options = WMSTileOptions(format = "image/png", transparent = F)) %>%
+        addWMSTiles("https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Reference/MapServer/tile/{z}/{y}/{x}.png",
+                    layers = "World_Ocean_Reference",
+                    group = "Ocean Reference",
+                    options = WMSTileOptions(format = "image/png", transparent = T)) %>%
+        addWMSTiles("https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?",
+                    layers = "GEBCO_LATEST",
+                    group = "GEBCO",
+                    options = WMSTileOptions(format = "image/png", transparent = F)) %>%
+        addProviderTiles(providers$Esri.WorldImagery,
                          group = "World Imagery") %>%
-        addLayersControl(baseGroups = c('Ocean Basemap', 'World Imagery')) %>%
+        addLayersControl(baseGroups = c('Ocean Basemap', 'GEBCO', 'World Imagery'),
+                         overlayGroups = c('Ocean Reference')) %>%
         addPolylines(
           lat = mapUp$lat,
           lng = mapUp$long,
--- modules/routing.R
+++ modules/routing.R
@@ -77,11 +77,23 @@ routing_server <- function(id, gliderName) {
         #print(userGoto)
         #base provider layers
         p <- leaflet() %>%
-          addProviderTiles(providers$Esri.OceanBasemap,
-                         group = "Ocean Basemap") %>%
+          #base provider layers
+          addWMSTiles("https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}.png",
+                      layers = "World_Ocean_Base",
+                      group = "Ocean Basemap",
+                      options = WMSTileOptions(format = "image/png", transparent = F)) %>%
+          addWMSTiles("https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Reference/MapServer/tile/{z}/{y}/{x}.png",
+                      layers = "World_Ocean_Reference",
+                      group = "Ocean Reference",
+                      options = WMSTileOptions(format = "image/png", transparent = T)) %>%
+          addWMSTiles("https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?",
+                      layers = "GEBCO_LATEST",
+                      group = "GEBCO",
+                      options = WMSTileOptions(format = "image/png", transparent = F)) %>%
           addProviderTiles(providers$Esri.WorldImagery,
                            group = "World Imagery") %>%
-          addLayersControl(baseGroups = c('Ocean Basemap', 'World Imagery')) %>%
+          addLayersControl(baseGroups = c('Ocean Basemap', 'GEBCO', 'World Imagery'),
+                           overlayGroups = c('Ocean Reference')) %>%
           addPolylines(
             lat = map_sf$lat,
             lng = map_sf$long,
@@ -143,11 +155,22 @@ routing_server <- function(id, gliderName) {
       
     p <- leaflet() %>%
       #base provider layers
-      addProviderTiles(providers$Esri.OceanBasemap,
-                       group = "Ocean Basemap") %>%
+      addWMSTiles("https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}.png",
+                  layers = "World_Ocean_Base",
+                  group = "Ocean Basemap",
+                  options = WMSTileOptions(format = "image/png", transparent = F)) %>%
+      addWMSTiles("https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Reference/MapServer/tile/{z}/{y}/{x}.png",
+                  layers = "World_Ocean_Reference",
+                  group = "Ocean Reference",
+                  options = WMSTileOptions(format = "image/png", transparent = T)) %>%
+      addWMSTiles("https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?",
+                  layers = "GEBCO_LATEST",
+                  group = "GEBCO",
+                  options = WMSTileOptions(format = "image/png", transparent = F)) %>%
       addProviderTiles(providers$Esri.WorldImagery,
                        group = "World Imagery") %>%
-      addLayersControl(baseGroups = c('Ocean Basemap', 'World Imagery')) %>%
+      addLayersControl(baseGroups = c('Ocean Basemap', 'GEBCO', 'World Imagery'),
+                       overlayGroups = c('Ocean Reference')) %>%
       addPolylines(
         lat = map_sf$lat,
         lng = map_sf$long,
