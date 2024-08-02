b9968fbe51fefeaa6be676a1b8cfc3d7fa30750a
Sukerfish <viper31jas@aim.com>
2024-08-02 10:34:14 -0400

    reorganization updates

--- batteryMarkdownGenerate.R
+++ batteryMarkdownGenerate.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(emayili)
 
 #get deployed gliders
-deployedGliders <- read.csv("/echos/processGliders.txt", 
+deployedGliders <- read.csv("/echos/processGliders.txt",
                             sep = "",
                             header = FALSE)
 colnames(deployedGliders)[1] = "Name"
@@ -13,24 +13,24 @@ deployedGliders <- deployedGliders %>%
   filter(!str_starts(Name,"#")) #remove any commented lines
 
 for (i in deployedGliders$Name){
-  
+
   #extract battery capacity as listed
   amps <- deployedGliders %>%
     filter(Name == i)
-  
+
   #pass glidername as parameter from i to each render block
   if (amps$ahrCap > 0){
     msg <- envelope() %>%
-      emayili::render("/echos/batteryMarkdown.Rmd", params = list(gliderName = paste0(i))) %>%
+      emayili::render("./Documentation/batteryMarkdown.Rmd", params = list(gliderName = paste0(i))) %>%
       subject(paste0("Daily summary for ", as.character(i)))
-    
+
     capture.output(print(msg, details = TRUE), file = paste0("/echos/", i, "/summary.html"))
   } else {
     msg <- envelope() %>%
-      emayili::render("/echos/batteryMarkdownNoAmp.Rmd", params = list(gliderName = paste0(i))) %>%
+      emayili::render("./Documentation/batteryMarkdownNoAmp.Rmd", params = list(gliderName = paste0(i))) %>%
       subject(paste0("Daily summary for ", as.character(i)))
-    
+
     capture.output(print(msg, details = TRUE), file = paste0("/echos/", i, "/summary.html"))
   }
-  
+
 }
